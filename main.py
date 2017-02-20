c0af7cc8dfe50ce1a23f31e4db5cc890e98ddb2d
Betsy Klee <Betsy Klee>
2017-02-20 17:39:52 -0600

    Commit Prior to Hacker Challenge

--- main.py
+++ main.py
@@ -15,11 +15,75 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class BlogHome(Handler):
+    def render_front(self):
+        posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5")
+        self.render("main-blog.html", posts=posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+class NewPost(Handler):
+    def render_front(self, title = "", body = "", error = ""):
+        self.render("new-blog.html", title = title, body = body, error = error)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            a = BlogPost(title = title, body = body)
+            a.put()
+            id = a.key().id()
+            self.redirect("/blog/%s" % str(a.key().id()))
+        else:
+            error = "We need both a title and a body!"
+            self.render_front(title, body, error = error)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        id = int(id)
+        post = BlogPost.get_by_id(id, parent = None)
+        if post:
+           self.render("post.html", post = post)
+        else:
+            error = "There is no post with that ID!"
+            self.write(error)
+
+class FrontPage(Handler):
+    def get(self):
+        self.redirect("/blog")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/', FrontPage),
+    ('/blog', BlogHome),
+    ('/blog/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', handler = ViewPostHandler, name = 'post')
+])
